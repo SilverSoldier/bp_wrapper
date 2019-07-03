56c73c3f54dd467542331c8fdad564ceb2afaae5
SilverSoldier <soldatargent@gmail.com>
2019-07-03 15:24:53 +0530

    Added functions for operations over commitments.
    Added test cases for same both in wrapper.rs and test.c

--- c/test.c
+++ c/test.c
@@ -3,8 +3,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
-int test_range_proof()
-{
+int test_range_proof() {
     uint64_t num = 210;
     char commitment[32] = { 0 };
     size_t range = 8;
@@ -16,43 +15,125 @@ int test_range_proof()
     if (act_ps != 0) {
 	/* printf("Expected: %d, Actual: %d\n", exp_ps, act_ps); */
 	bool verify = verify_proof(proof, act_ps, commitment, range);
-	if (verify != 0) {
-	    printf("Successfully verified proof.\n");
+    return verify;
 	}
-    } else {
-	printf("Error generating proof.\n");
-    }
-    return 0;
+	return 0;
 }
 
 int test_gen_commitment() {
+  int i;
   char num1[32] = { 0 };
+  char num2[32] = { 0 };
   char zero[32] = { 0 };
   char comm1[32] = { 0 };
+  char comm2[32] = { 0 };
 
-  int i;
-  for(i = 0; i < 32; i++){
-	num1[i] = 10;
-  	printf("%d\t", num1[i]);
-  }
-
-  printf("\n");
-
-  fflush(stdout);
-
+  num1[10] = 10;
+  num2[10] = 10;
+  
   gen_commitment(num1, zero, comm1);
-  /* gen_commitment(num2, zero, comm2); */
-
-  printf("\n\n");
+  gen_commitment(num2, zero, comm2);
 
   for(i = 0; i < 32; i++){
-	printf("%d \t", comm1[i]);
+	if (comm1[i] != comm2[i]) {
+	  return 0;
+	}
   }
+  return 1;
+}
+
+int test_add_comm() {
+  int i;
+  char val10[32] = { 0 };
+  char val20[32] = { 0 };
+  char zero[32] = { 0 };
+  char comm10[32] = { 0 };
+  char exp_comm20[32] = { 0 };
+  char act_comm20[32] = { 0 };
+
+  // Put values in necessary arrays
+  val10[0] = 10;
+  val20[0] = 20;
+
+  // Generate commitments for 10 and 20
+  gen_commitment(val10, zero, comm10);
+  gen_commitment(val20, zero, exp_comm20);
+
+  // Generate multiplication of 10 and 2
+  add_commitment(comm10, comm10, 0, act_comm20);
+  
+  for(i = 0; i < 32; i++){
+	if (exp_comm20[i] != act_comm20[i]) {
+	  return 0;
+	}
+  }
+  return 1;
+} 
+
+int test_sub_comm() {
+  int i;
+  char val10[32] = { 0 };
+  char zero[32] = { 0 };
+  char comm10[32] = { 0 };
+  char comm0[32] = { 0 };
+
+  // Put values in necessary arrays
+  val10[0] = 10;
+
+  // Generate commitments for 10 and 20
+  gen_commitment(val10, zero, comm10);
+
+  // Generate multiplication of 10 and 2
+  add_commitment(comm10, comm10, 1, comm0);
+  
+  for(i = 0; i < 32; i++){
+	if (comm0[i] != zero[i]) {
+	  return 0;
+	}
+  }
+  return 1;
+} 
+
+int test_mult_comm() {
+  int i;
+  char val10[32] = { 0 };
+  char val20[32] = { 0 };
+  char zero[32] = { 0 };
+  char comm10[32] = { 0 };
+  char exp_comm20[32] = { 0 };
+  char act_comm20[32] = { 0 };
+
+  // Put values in necessary arrays
+  val10[0] = 10;
+  val20[0] = 20;
+
+  // Generate commitments for 10 and 20
+  gen_commitment(val10, zero, comm10);
+  gen_commitment(val20, zero, exp_comm20);
+
+  // Generate multiplication of 10 and 2
+  mult_commitment(comm10, 2, act_comm20);
+  
+  for(i = 0; i < 32; i++){
+	if (exp_comm20[i] != act_comm20[i]) {
+	  return 0;
+	}
+  }
+  return 1;
 }
 
 int main()
 {
   /* test_range_proof(); */
-  test_gen_commitment();
+  printf("");
+  printf("Generate Commitment Equality Test: %s\n", test_gen_commitment() ? "Passed" : "Failed");
+
+  printf("Add Commitment Test: %s\n", test_add_comm() ? "Passed" : "Failed");
+
+  printf("Sub Commitment Test: %s\n", test_sub_comm() ? "Passed" : "Failed");
+
+  printf("");
+  printf("Mult Commitment Test: %s\n", test_mult_comm() ? "Passed" : "Failed");
+
   return 0;
 }
--- src/wrapper.rs
+++ src/wrapper.rs
@@ -3,7 +3,7 @@ extern crate merlin;
 extern crate rand;
 
 use bulletproofs::{BulletproofGens, PedersenGens, RangeProof};
-use wrapper::curve25519_dalek::{scalar::Scalar, ristretto::CompressedRistretto};
+use wrapper::curve25519_dalek::{scalar::Scalar, ristretto::CompressedRistretto, ristretto::RistrettoPoint};
 use self::merlin::Transcript;
 use self::rand::{thread_rng};
 use std::slice;
@@ -115,46 +115,160 @@ pub extern "C" fn gen_commitment(value: *const u8, blinding: *const u8, commitme
         return
     }
 
-    let value_bytes = unsafe {
+    let mut value_array = [0; 32];
+    let mut blinding_array = [0; 32];
+
+    let val_scalar = unsafe {
         if value.is_null() {
             println!("Value is null.");
             return
         }
-        slice::from_raw_parts(value, 32)
+        let value_bytes = slice::from_raw_parts(value, 32);
+        value_array.copy_from_slice(value_bytes);
+        Scalar::from_bytes_mod_order(value_array)
     };
 
-    let blinding_bytes = unsafe {
+    let blinding_scalar = unsafe {
         if blinding.is_null() {
-            println!("Blinding is null.");
-            return
+            Scalar::zero();
         }
-        slice::from_raw_parts(blinding, 32)
+        let blinding_bytes = slice::from_raw_parts(blinding, 32);
+        blinding_array.copy_from_slice(blinding_bytes);
+        Scalar::from_bytes_mod_order(blinding_array)
     };
-
-    let mut value_array = [0; 32];
-    let mut blinding_array = [0; 32];
-
-    println!("{:?}", value_bytes);
-
-    value_array.copy_from_slice(value_bytes);
-    blinding_array.copy_from_slice(blinding_bytes);
 
     let pc_gens = PedersenGens::default();
 
-    let val_scalar = Scalar::from_bits(value_array);
-    let blinding_scalar = Scalar::from_bits(blinding_array);
-
     let commitment = pc_gens.commit(val_scalar, blinding_scalar).compress();
-
-    println!("{:?}", commitment);
 
     unsafe {
         *commitment_return = commitment.to_bytes();
     };
 }
+
+fn op_comm_helper(comm: *const u8) -> Option<RistrettoPoint> {
+    let comm_bytes = unsafe {
+        if comm.is_null() {
+            println!("Commitment is null.");
+            return None;
+        }
+        slice::from_raw_parts(comm, 32)
+    };
+
+    CompressedRistretto::from_slice(comm_bytes).decompress() 
+
+}
+
+/** Function to perform add/sub operation on the Pedersen Commitments.
+ * Input:
+comm1: operand 1
+comm2: operand 2
+op: the operation to perform (0: +, 1: -)
+ */ 
+#[no_mangle]
+pub extern "C" fn add_commitment(comm1: *const u8, comm2: *const u8, op: i32, commitment_return: *mut [u8;32]) -> bool {
+    let comm1_value = match op_comm_helper(comm1) {
+        Some(val) => val,
+        None => {
+            println!("Error reading commitment");
+            return false
+        }
+    };
     
+    let comm2_value = match op_comm_helper(comm2) {
+        Some(val) => val,
+        None => {
+            println!("Error reading commitment");
+            return false
+        }
+    };
+
+    let comm_result = match op {
+        0 => comm1_value + comm2_value,
+        1 => comm1_value - comm2_value,
+        _ => {
+            println!("Operation not defined. Only input 0 or 1");
+            return false
+        }
+    };
+
+    unsafe {
+        *commitment_return = comm_result.compress().to_bytes();
+    }
+    return true;
+}
+
+#[no_mangle]
+pub extern "C" fn mult_commitment(comm1: *const u8, scalar: i32, commitment_return: *mut [u8;32]) {
+    let comm1_value = match op_comm_helper(comm1) {
+        Some(val) => val,
+        None => {
+            println!("Error reading commitment");
+            return 
+        }
+    };
+
+    let mut scalar_bytes: [u8; 32] = [0;32];
+    scalar_bytes[..4].copy_from_slice(&scalar.to_le_bytes());
+
+    let scalar_value = Scalar::from_bytes_mod_order(scalar_bytes);
+    let result = comm1_value * scalar_value;
+
+    unsafe {
+        *commitment_return = result.compress().to_bytes();
+    }
+
+}
+
 // #[no_mangle]
 // pub extern "C" fn gen_commitment0(value: [u8;32], commitment_return: *mut [u8;32]) {
 //     let zero = Scalar::zero();
 //     gen_commitment(value, zero.to_bytes(), commitment_return);
 // }
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_comm_eq() {
+        let pc_gens = PedersenGens::default();
+
+        let mut val1 = [0; 32];
+        val1[0] = 10;
+        
+        let mut val2 = [0; 32];
+        val2[0] = 10;
+
+        let blinding = Scalar::zero();
+
+        let comm1 = pc_gens.commit(Scalar::from_bytes_mod_order(val1), blinding).compress();
+
+        let comm2 = pc_gens.commit(Scalar::from_bytes_mod_order(val2), blinding).compress();
+
+        assert_eq!(comm1, comm2);
+    }
+
+
+    #[test]
+    fn test_comm_add() {
+        let pc_gens = PedersenGens::default();
+
+        let mut val1 = [0; 32];
+        val1[31] = 10;
+        
+        let mut val2 = [0; 32];
+        val2[31] = 20;
+
+        let blinding = Scalar::zero();
+
+        let comm1 = pc_gens.commit(Scalar::from_bytes_mod_order(val1), blinding);
+
+        let comm2 = pc_gens.commit(Scalar::from_bytes_mod_order(val2), blinding);
+
+        let comm3 = comm2 - comm1 - comm1;
+
+        assert_eq!(comm3.compress().to_bytes(), [0;32]);
+    }
+}
